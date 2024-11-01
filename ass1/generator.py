a29180850080fc5ec6536df100edbb2f2b21b370
yalaka03 <82953352+yalaka03@users.noreply.github.com>
2024-11-01 19:44:36 +0530

    ass1 Source added

---
+++ ass1/generator.py
@@ -0,0 +1,29 @@
+from ngram import NGramModel
+from tokenizer_util import tokenize
+import sys
+if len(sys.argv) > 1:
+
+    arguments = sys.argv[1:]
+    for arg in arguments:
+        print(arg)
+    good_turing = False
+    interpolation = False
+    n = 3
+    if "g" in arguments:
+        good_turing = True
+    if "i" in arguments:
+        interpolation = True
+    file_path = arguments[-2]
+    k = int(arguments[-1])
+    model = NGramModel(file_path, n, interpolation, good_turing)
+    model.setup()
+    model.train_model()
+    sentence = input("input sentence:")
+    sentence = tokenize(sentence)[0]
+    print(sentence)
+    words = model.generate(sentence,k)
+    for word in words:
+        print(word[0],word[1])
+
+else:
+    print("No arguments provided.")
